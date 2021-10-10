ba70fcce11a43c5e511ef1ef2dbad9ebb5a95b20
MarcoDelMondo <43528263+MarcoDelMondo@users.noreply.github.com>
2021-10-10 14:53:11 -0700

    Lab 3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+Ben Carrier 300290267
 
-from flask import Flask
-import os
+from flask import Flask, request, redirect
+from datetime import datetime
+import os, json
 from google.cloud import datastore
 
 app = Flask(__name__)
@@ -47,6 +49,47 @@ def getversionid():
     versionid = os.getenv('GAE_VERSION')
     return str(versionid)
 
+@app.route('/editor')
+def edit_page():
+    with open('editor.html', 'r') as page:
+        return page.read()
 
+@app.route('/submit', methods = ['POST'])
+def submit_post():
+    password = request.form['pass']
+    if password == "mySuperAwesomePassword":
+        content = request.form['content']
+        title = request.form['title']
+        time = str(datetime.utcnow())
+        post = json.dumps([content, title, time])
+        ent = dataclient.key('data', 'posts')
+        posts = dataclient.get(key=ent)
+        if posts:
+            posts['posts'] = [post] + posts['posts']
+            dataclient.put(posts)
+        else:
+            posts = dataclient.Entity(key=ent)
+            dataclient.put(posts)
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route('/')
+def main_page():
+    ent = dataclient.key('data', 'posts')
+    posts = dataclient.get(key=ent)
+    article = ""
+    with open('article.html', 'r') as page:
+        article = page.read()
+    html = ""
+    if posts:
+        for post in posts['posts']:
+            array = json.loads(post)
+            raw = article.replace("!content!", array[0])
+            raw = raw.replace("!title!", array[1])
+            raw = raw.replace("!time!", array[2])
+            html += raw
+    else:
+        return 'No Posts!'
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
